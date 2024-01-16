2ba7c1be5d88909f1c2a4c109f673f7ed1b4891f
Akshay Mathur <avi.mathur.engg+github@gmail.com>
2024-01-16 16:38:23 +0000

    Support for posting comments

--- website/pages/forum.py
+++ website/pages/forum.py
@@ -15,7 +15,7 @@ class Comment:
 class ForumThread:
     _id: uuid4
     post_date: datetime
-    question: str 
+    question: str
     answer: str
     comments: list[Comment]
 
@@ -36,40 +36,55 @@ def post_thread(question:str, answer:str):
 def add_thread_to_forum(thread: ForumThread):
     threads_collection = client.test.threads
     threads_collection.insert_one(asdict(thread))
-    render_forum()
-    
+
 def add_comment_to_forum(thread: ForumThread, comment: Comment):
     threads_collection = client.test.threads
     thread.comments.append(comment)
-    threads_collection.find_one_and_update(thread)
-    render_forum()
+    return threads_collection.find_one_and_update(filter={'_id': thread._id}, update={
+                                           '$push': {'comments': asdict(comment)}}, upsert=True)
 
 # Pull data from the collection.
-# Uses st.cache_data to only rerun when the query changes or after 1 min.
-# @st.cache_data(ttl=60)
+# Uses st.cache_data to only rerun when the query changes or after ttl 
+@st.cache_data(ttl=10)
 def read_forum_data():
     items = client.test.threads.find()
     items = list(items)  # make hashable for st.cache_data
     threads = [ForumThread(**item) for item in items]
     return threads
-    
+
 def render_forum():
     threads = read_forum_data()
     with st.container(border=True):
-            st.markdown("# All Threads")
-            for thread in threads:
-                with st.container(border=True):
-                    st.markdown("*{}*".format(thread.post_date.date()))
-                    st.markdown("## Q: {}".format(thread.question['content']))
-                    st.markdown("### A: {} ".format(thread.answer['content']))
-                    with st.expander("💬 Open comments"):
-                        # Show comments
-                        st.write("**Comments:**")
-                        for comment in thread.comments:
-                            st.markdown("{} - {}> {}".format(comment.userid, comment.post_date, comment.comment))
-                        if len(thread.comments) > 0:
-                            st.success("☝️ Your comment was successfully posted.")
+        st.markdown("# All Threads")
+        for thread in threads:
+            with st.container(border=True):
+                st.markdown("*{}*".format(thread.post_date.date()))
+                st.markdown("## Q: {}".format(thread.question['content']))
+                st.markdown("### A: {} ".format(thread.answer['content']))
+                comment_box = st.expander("💬 Open comments")
+                # Show comments
+                comment_box.write("**Comments:**")
+                for comment_dict in thread.comments:
+                    comment = Comment(**comment_dict)
+                    comment_box.markdown("{} - {}> {}".format(comment.userid,
+                                comment.post_date, comment.comment))
+                with st.form("comment_box" + str(thread._id), clear_on_submit=True, border=True):
+                    comment_text = st.text_input("Enter comment here")
+                    submitted = st.form_submit_button(
+                        ":green[Post Comment]")
+                    if submitted:
+                        if comment_text == "":
+                            st.toast(":red[☝️ Your comment cannot be empty.]")
+                        else:
+                            comment = Comment(
+                                _id=uuid4(), comment=comment_text, post_date=datetime.now(), userid="test_user")
+                            if add_comment_to_forum(thread, comment):
+                                comment_box.write(
+                                    "{} - {}> {}".format(comment.userid, comment.post_date, comment.comment))
+                                st.toast(":green[☝️ Your comment was successfully posted.]")
 
 if 'forum_render' in st.session_state:
     st.title("📝 Gurukul Forum")
     render_forum()
+
+set_rendering_on()
