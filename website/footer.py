9de34f5213859f8c2a41508403026802bbf3b3a5
Krishan Subudhi <krishan.subudhi@gmail.com>
2024-01-22 12:41:43 +0530

    added footer (#11)

---
+++ website/footer.py
@@ -0,0 +1,21 @@
+# Footer
+footer_html = """
+---
+**Developers:**
+
+<a href = "https://www.linkedin.com/in/krishan-subudhi-4521212b/"><img src="https://media.licdn.com/dms/image/D5603AQHliJD8ppFCWQ/profile-displayphoto-shrink_200_200/0/1686448453389?e=1711584000&v=beta&t=kMyvseBFfH7Xljf-ejyASyFb3qjd1wZXGqKmp5bw5Dw" alt="Your Image" width="50" height="50" style="float: left; margin-right: 10px; border-radius: 50%;" ></a>
+
+[Krishan Subudhi](https://www.linkedin.com/in/krishan-subudhi-4521212b/) - 
+Software Engineer @ Google
+
+<a href = "https://www.linkedin.com/in/akshaymathurijetr/"><img src="https://media.licdn.com/dms/image/C4E03AQGAMXH4xvM4Yg/profile-displayphoto-shrink_200_200/0/1516437332705?e=1711584000&v=beta&t=GE6Iq58TYbTrSzvFmFQNcCeA11KEQLxTKOnqeukdWYA" alt="Your Image" width="50" height="50" style="float: left; margin-right: 10px; border-radius: 50%;" ></a>
+
+[Akshay Mathur](https://www.linkedin.com/in/akshaymathurijetr/) - 
+Software Engineer @ Google
+
+---
+
+* Copyright &copy; Gurukul
+* All Rights Reserved
+
+"""
